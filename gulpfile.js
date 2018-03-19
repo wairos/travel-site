ab5b82621dc0a304bb3eada16e3e4584dea92ca1
Lukas Waire <wairelm@gmail.com>
2018-03-18 22:16:48 -0500

    latest lesson Lets Build

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,13 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-browserSync = require('browser-sync').create();
-
-gulp.task('default', function(){
-  console.log('Hooray - gulp task complete');
-});
-
-gulp.task('styles', function(){
-
-  return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-  .pipe(gulp.dest('./app/temp/styles'));
-
-});
-
-gulp.task('watch', function(){
-
-  watch('./app/index.html', function(){
-    browserSync.reload();
-  });
-
-//watches any css file in /styles or in any sub-folder of /styles
-  watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('cssInject');
-  });
-
-  browserSync.init({
-    notify: false,
-    server: {
-      baseDir: "app"
-    }
-  });
-
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
 
-gulp.task('cssInject', ['styles'], function(){
-  return gulp.src('./app/temp/styles/styles.css').pipe(browserSync.stream());
-});
+
+
+
+
+
+
+
+
+
